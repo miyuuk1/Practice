c39f5c8e9281a4ac80b42a9d3109ff86b58a7c60
miyuuk1 <50059433+miyuuk1@users.noreply.github.com>
2019-07-15 23:18:18 +0300

    Update WebApp.java

--- src/main/java/ki/miyuu/webservice/WebApp.java
+++ src/main/java/ki/miyuu/webservice/WebApp.java
@@ -1,13 +1,9 @@
 package ki.miyuu.webservice;
 
 import java.util.ArrayList;
-
-// Импортируем необходимые библиотеки
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-
-// Определяем путь по которому будет доступен сервис
 @Path("/ratings")
 public class WebApp {
 	
